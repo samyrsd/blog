efd3e6c40620cf685a28921e57fab3c4ebdbdf78
samyrsd <yrsamrith@gmail.com>
2019-05-27 15:59:01 +0900

    Add questions.md

--- src/pages/who-am-i.js
+++ src/pages/who-am-i.js
@@ -6,7 +6,7 @@ export default ({ data }) => (
   <Layout>
     <h1>Hi</h1>
     <p>
-      I'm Sam. Random guys on the Internet. 
+      I'm Sam. A random guy on the Internet. 
       You will find most of the things here are what I don't know and what's fun. 
       Because I figured the best way the learn is to explain and the best way to have fun is to create fun stuff.
       Life's a bit boring sometimes. Fun is not optional anymore. <span role="img" aria-label="smile">&#128540;</span>
